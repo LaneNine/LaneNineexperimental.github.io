fd613b7bc687160b7d8c5f12b549e8a589f9d30f
LaneNine <fkscmmr@gmail.com>
2026-08-01 20:57:32 -0500

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,43 @@
+const EventManager = {
+    getEvents() {
+        return Object.keys(StorageManager.getData().events);
+    },
 
+    create(name) {
+        if (!name) return false;
+        StorageManager.addEvent(name);
+        return true;
+    },
+
+    remove(name) {
+        StorageManager.removeEvent(name);
+    },
+
+    getCurrent() {
+        return StorageManager.getData().settings.selectedEvent;
+    },
+
+    setCurrent(name) {
+        const data = StorageManager.getData();
+
+        if (!data.events[name]) return false;
+
+        data.settings.selectedEvent = name;
+        StorageManager.save();
+        return true;
+    },
+
+    getSwims(name = this.getCurrent()) {
+        const event = StorageManager.getEvent(name);
+        return event ? event.swims : [];
+    },
+
+    getGoal(name = this.getCurrent()) {
+        const event = StorageManager.getEvent(name);
+        return event ? event.goal : null;
+    },
+
+    addSwim(swim, name = this.getCurrent()) {
+        StorageManager.addSwim(name, swim);
+    }
+};
