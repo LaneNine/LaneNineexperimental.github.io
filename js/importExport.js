09ecb2a4ca4d7a202c0329bb55f4d11537ebdec5
LaneNine <fkscmmr@gmail.com>
2026-08-01 20:59:27 -0500

    Update importExport.js

--- js/importExport.js
+++ js/importExport.js
@@ -1,1 +1,34 @@
 
+const ImportExport = {
+    download() {
+        const data = StorageManager.export();
+        const blob = new Blob([data], {
+            type: "application/json"
+        });
+
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+
+        link.href = url;
+        link.download = "lane9-backup.json";
+        link.click();
+
+        URL.revokeObjectURL(url);
+    },
+
+    upload(file) {
+        return new Promise((resolve) => {
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                const success = StorageManager.import(
+                    reader.result
+                );
+
+                resolve(success);
+            };
+
+            reader.readAsText(file);
+        });
+    }
+};
